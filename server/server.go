9a25b5765cbdaa63b7c5ead7852a89f18f488647
Baojia Zhang <32551792+bjzhang1101@users.noreply.github.com>
2022-06-21 14:36:21 +0800

    Update comments

--- server/server.go
+++ server/server.go
@@ -14,18 +14,16 @@ const (
 	maxRequestBodySize = 4 * 1024 * 1024
 )
 
-// Handler handles admission review mutation requests.
+// Handler handles requests.
 type Handler struct{}
 
-// Handle processes a mutation admission review request.
-//
-// The request content type must be application/json.
+// Handle processes a HTTP request to the raft server.
 func (h *Handler) Handle(ctx *fasthttp.RequestCtx) {
 	ctx.Response.Header.SetContentType(contentType)
 	ctx.SetStatusCode(200)
 }
 
-// NewServer creates an HTTP server for serving admission review requests.
+// NewServer creates an HTTP server serving requests.
 func NewServer() (*fasthttp.Server, error) {
 	handler := Handler{}
 
